a8ab6912a715e2f01e6733b9e6db77ee50668d4e
KseandI <KseandI@gmail.com>
2021-02-25 19:35:58 +0300

    Added title

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,12 @@
 extern crate gtk;
-use gtk::prelude::*;
-use gtk::{ButtonsType, DialogFlags, MessageDialog, MessageType, Window};
 
 mod mfgx_inst;
 
 fn main() {
-    let mut mfgx = mfgx_inst::Mfgx::new();
     if gtk::init().is_err() {
         println!("Failed to initialize GTK.");
         return;
     }
-    let msd: MessageDialog = MessageDialog::new(
-        None::<&Window>,
-        DialogFlags::empty(),
-        MessageType::Info,
-        ButtonsType::Ok,
-        "Hello World",
-    );
-    msd.run();
+    let mut mfgx = mfgx_inst::Mfgx::new(800, 600, "/");
+    mfgx.run();
 }
--- src/mfgx_inst.rs
+++ src/mfgx_inst.rs
@@ -1,11 +1,40 @@
+extern crate gtk;
+extern crate gio;
+
+// To import all needed traits.
+use gtk::{GtkWindowExt, prelude::*};
+use gio::prelude::*;
+
 pub struct Mfgx {
-    path: String,
+    path: &'static str,
+    window: gtk::Application,
 }
 
 impl Mfgx {
-    pub fn new() -> Mfgx {
-        Mfgx {
-            path: String::from("/"),
-        }
+    pub fn new(x: i32, y: i32, _path: &'static str) -> Mfgx {
+        let mut mfgx: Mfgx = Mfgx {
+            path: _path,
+            window: gtk::Application::new(Some("org.kseandi.mfgx"),
+                                          gio::ApplicationFlags::FLAGS_NONE)
+                                      .expect("Application::new failed :("),
+        };
+        mfgx.window.connect_activate(move |app| {
+            let win = gtk::ApplicationWindow::new(app);
+            let hb = gtk::HeaderBar::new();
+
+            win.set_default_size(x, y);
+            win.set_titlebar(Some(&hb));
+
+            hb.set_show_close_button(true);
+
+            hb.set_title(Some("Mfgx"));
+            hb.set_subtitle(Some(_path));
+
+            win.show_all();
+        });
+        mfgx
+    }
+    pub fn run(self: &mut Mfgx) {
+        self.window.run(&[]);
     }
 }
