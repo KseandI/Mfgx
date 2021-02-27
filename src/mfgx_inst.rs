06c82aedbaf868a3a02dd419b6a1a6e7b3980298
KseandI <KseandI@gmail.com>
2021-02-27 12:42:49 +0300

    Major Update

--- src/mfgx_inst.rs
+++ src/mfgx_inst.rs
@@ -22,22 +22,46 @@ impl Mfgx {
         let header: gtk::HeaderBar = gtk::HeaderBar::new();
         let window: gtk::ApplicationWindow = gtk::ApplicationWindow::new(application);
         let panel: gtk::Paned = gtk::Paned::new(gtk::Orientation::Horizontal);
-        let head_menu: gtk::MenuButton = gtk::MenuButton::new();
+        let add_folder: gtk::Button = gtk::Button::new();
+        let add_file: gtk::Button = gtk::Button::new();
         let folder_view: gtk::FlowBox = gtk::FlowBox::new();
-        let test_fldr: gtk::Image = gtk::Image::from_icon_name(
-            Some("folder"), 
-            gtk::IconSize::Button);
-        let test_button: gtk::Button = gtk::Button::with_label("Console?");
-        window.resize(800, 600);
+        let folder_view_entity: gtk::FlowBoxChild = gtk::FlowBoxChild::new();
+        let selected_entity: gtk::Image = gtk::Image::from_icon_name(
+            Some("gtk-directory"), 
+            gtk::IconSize::Dialog);
+        let selected_text: gtk::Label = gtk::Label::new(Some("Directory"));
+        let selected_info: gtk::Label = gtk::Label::new(Some("..."));
+        let panel_vbox: gtk::Box = gtk::Box::new(gtk::Orientation::Vertical, 5);
+        window.set_property_default_width(800);
+        window.set_property_default_height(600);
+
+        add_folder.add(&gtk::Image::from_icon_name(
+            Some("folder-new"),
+            gtk::IconSize::SmallToolbar));
+        add_file.add(&gtk::Image::from_icon_name(
+            Some("document-new"),
+            gtk::IconSize::SmallToolbar));
 
         header.set_show_close_button(true);
         header.set_title(Some("Mfgx"));
-        header.pack_end(&head_menu);
+        header.pack_start(&add_folder);
+        header.pack_start(&add_file);
         window.set_titlebar(Some(&header));
         
-        panel.add(&test_button);
+        panel.add(&panel_vbox);
+        panel_vbox.add(&selected_entity);
+
+        panel_vbox.set_margin_start(5);
+        panel_vbox.set_margin_end(5);
+
+        panel_vbox.add(&selected_text);
+
+        selected_info.set_justify(gtk::Justification::Center);
+        selected_info.set_hexpand(true);
+        selected_info.set_vexpand(true);
+        panel_vbox.add(&selected_info);
  
-        folder_view.insert(&test_fldr, -1);
+        folder_view.insert(&folder_view_entity, -1);
         folder_view.set_property_expand(true);
         panel.add(&folder_view);
 
